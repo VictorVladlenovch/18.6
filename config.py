6eca0e6f88e3b17e3527e5ff79f84c1846b405d9
VictorVladlenovch <121546893+VictorVladlenovch@users.noreply.github.com>
2023-02-03 11:07:56 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+TOKEN = "5854742686:AAG2Ph-1VkIExtVntGX1f1SDb3hlRrz8vig"
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUR',
+    'евро': 'EUR'
+}
+
